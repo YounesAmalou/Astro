ef1fd58960aa9c2a4153d103e5834ac58e3af5ed
YounesAmalou <amalou.younes2002@gmail.com>
2023-09-12 12:48:19 +0100

    changed the script (the rotation formula) some animation positioned the elements to be compatible with most of the screen sizes next task: optimise the slow animation (seems to be due to the description animation) loading planets and description is now dynamic

---
+++ assets/js/planets.js
@@ -0,0 +1,57 @@
+// Planets Data:
+const planets = [
+    {
+        name: "Earth",
+        description: "Earth is the third planet from the Sun and home to all known life. While large volumes of water can be found throughout the Solar System, only Earth sustains liquid surface water. Approximately 70.8% of Earth's surface is made up of the ocean, dwarfing Earth's polar ice, lakes, and rivers. The remaining 29.2% of Earth's surface is land.",
+        src: "assets/images/Earth.svg",
+    },
+    {
+        name: "Moon",
+        description: "The Moon is an astronomical body orbiting Earth as its only natural satellite. It is the fifth-largest satellite in the Solar System, and by far the largest among planetary satellites relative to the size of the planet that it orbits. The Moon is, after Jupiter's satellite Io, the second-densest satellite in the Solar System among those whose densities are known. The Moon is thought to have formed about 4.51 billion years ago, not long after Earth. The most widely accepted explanation is that the Moon formed from the debris left over after a giant impact between Earth and a Mars-sized body called Theia.",
+        src: "assets/images/Moon.svg"
+    },
+    {
+        name: "Mercury",
+        description: "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury orbits the Sun within Earth's orbit as an inferior planet; its apparent distance from the Sun as viewed from Earth never exceeds 28°. From Earth, the planet telescopically displays the complete range of phases, similar to Venus and the Moon, which recurs over its synodic period of approximately 116 days. Mercury is most often the closest planet to Earth.",
+        src: "assets/images/Mercury.svg"
+    },
+    {
+        name: "Venus",
+        description: "Venus is the second planet from the Sun. It is sometimes called Earth's 'sister' or 'twin' planet as it is almost as large and has a similar composition. As an interior planet to Earth, Venus appears in Earth's sky never far from the Sun, either as morning star or evening star. Aside from the Sun and Moon, Venus is the brightest natural object in Earth's sky, capable of casting visible shadows on Earth at dark conditions and being visible to the naked eye in broad daylight.",
+        src: "assets/images/Venus.svg"
+    },
+    {
+        name: "Mars",
+        description: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, larger only than Mercury. In the English language, Mars is named for the Roman god of war. Mars is a terrestrial planet with a thin atmosphere and has a crust primarily composed of elements similar to Earth's crust, as well as a core made of iron and nickel. Mars has surface features such as impact craters, valleys, dunes, and polar ice caps. Mars has two small, irregularly shaped moons, Phobos and Deimos.",
+        src: "assets/images/Mars.svg"
+    },
+    {
+        name: "Jupiter",
+        description: "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass more than two and a half times that of all the other planets in the Solar System combined, while being slightly less than one-thousandth the mass of the Sun. Jupiter is the third brightest natural object in Earth's night sky.",
+        src: "assets/images/Jupiter.svg",
+        width: 200
+    },
+    {
+        name: "Saturn",
+        description: "Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth. It has only one-eighth the average density of Earth, but is over 95 times more massive. Saturn's interior is most likely composed of a rocky core, surrounded by a deep layer of metallic hydrogen, an intermediate layer of liquid hydrogen and liquid helium, and finally, a gaseous outer layer. Saturn has a pale yellow hue due to ammonia crystals in its upper atmosphere.",
+        src: "assets/images/Saturn.svg",
+        width: 250
+    },
+    {
+        name: "Uranus",
+        description: "Uranus is the seventh planet from the Sun. Uranus has the third-largest planetary radius and fourth-largest planetary mass in the Solar System. The planet is similar in composition to Neptune, and both have bulk chemical compositions which differ from those of the other two giant planets, Jupiter and Saturn (the gas giants). For this reason, scientists often distinguish Uranus and Neptune as 'ice giants'.",
+        src: "assets/images/Uranus.svg",
+        width: 250
+    },
+    {
+        name: "Neptune",
+        description: "Neptune is the eighth planet from the Sun and the farthest known planet in the Solar System. It is the fourth-largest planet in the Solar System by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, and slightly more massive than its near-twin Uranus.",
+        src: "assets/images/Neptune.svg",
+        width: 190
+    },
+    {
+        name: "Pluto",
+        description: "Pluto is a dwarf planet in the Kuiper belt, a ring of bodies beyond the orbit of Neptune. It is the ninth-largest and tenth-most-massive known object to directly orbit the Sun. It is the largest known trans-Neptunian object by volume, by a small margin, but is slightly less massive than Eris. Like other Kuiper belt objects, Pluto is made primarily of ice and rock and is much smaller than the inner planets. Pluto has only one-sixth the mass of Earth's moon, and one-third its volume.",
+        src: "assets/images/Pluto.svg"
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,106 +1,93 @@
-document.addEventListener("DOMContentLoaded", function() {
-    //Init dom selection
-    const planetsContainer = $("#planets-container");
-    const planetsDescriptionContainer = $("#planets-description-container");
-    
-    if (planetsContainer === undefined) {
-        console.error("Planets container not found")
-    }
-    if (planetsDescriptionContainer === undefined) {
-        console.error("Planets description container not found")
-    }
-    
-    const planets = Array.from(planetsContainer.children);
-    const descriptions = Array.from(planetsDescriptionContainer.children);
-    
-    if (planets.length !== descriptions.length) {
-        console.error("Planets and descriptions length don't match");
-        return;
-    }
-    if (!planets.length) {
-        console.error("No planets/Description found");
-        return;
-    }
-    
-    //Init Constants
-    const DEGREE = 360 / planets.length;
-    const SPEED = 10;
+const planetsContainer = $("#planets-container");
+const planetsDescriptionContainer = $("#planets-description-container");
 
-    //Planets
-    const PLANET_SIZE = planets[0].clientWidth; //take the planet with the default width
-    const PLANET_CONTAINER_CENTER_X = (planetsContainer.clientWidth - PLANET_SIZE) / 2;
-    const PLANET_CONTAINER_CENTER_Y = (planetsContainer.clientHeight - PLANET_SIZE) / 2;
-    const PLANET_CONTAINER_RADIUS = PLANET_CONTAINER_CENTER_X;
-    //OR const PLANET_CONTAINER_RADIUS = Math.min(PLANET_CONTAINER_CENTER_X, PLANET_CONTAINER_CENTER_Y);
+planetsContainer.innerHTML = planets.map((planet) => {
+    return `<img src="${planet.src}" alt="${planet.name}">`;
+}).join('');
+const planetImages = Array.from(planetsContainer.children);
+planets.forEach((planet, i) => {if(planet.width) planetImages[i].style.width = planet.width + "px"})
+planets.reverse();
+planets.unshift(planets.pop());
+planetsDescriptionContainer.innerHTML = planets.map((planet) => {
+    return `<div class="planet-description ${planet.name.toLowerCase()}" >
+                <h1>${planet.name}</h1>
+                <p>${planet.description}</p>
+            </div>`;
+}).join('');
+const planetDescriptions = Array.from(planetsDescriptionContainer.children);
 
-    //Descriptions
-    const DESCRIPTION_SIZE = descriptions[0].clientWidth; //take the description with the default width
-    const DESCRIPTION_CONTAINER_CENTER_X = (planetsDescriptionContainer.clientWidth - DESCRIPTION_SIZE) / 2;
-    const DESCRIPTION_CONTAINER_CENTER_Y = (planetsDescriptionContainer.clientHeight - DESCRIPTION_SIZE) / 2;
-    const DESCRIPTION_CONTAINER_RADIUS = DESCRIPTION_CONTAINER_CENTER_X*3;
-    //OR const DESCRIPTION_CONTAINER_RADIUS = Math.min(DESCRIPTION_CONTAINER_CENTER_X, DESCRIPTION_CONTAINER_CENTER_Y);
+let visibleImageIndex = 0, visibleDescriptionIndex = 0;
+planetImages[visibleImageIndex].classList.add('visible');
+planetDescriptions[visibleDescriptionIndex].classList.add('visible');
 
-    let currentDegree = 90;//Also starting degree (change it if you want to change the initial rotation)
+const count = planetImages.length;
+const STEP = 360 / count;
+let target = 90, current = 90, direction = 1, rotating = false;
 
-    addWidthToPlanetsWithCircle(['Saturn', 'Uranus'], planets);
-
-    setupPosition(planets, DEGREE, currentDegree, PLANET_CONTAINER_CENTER_X, PLANET_CONTAINER_CENTER_Y, PLANET_CONTAINER_RADIUS);
-    setupPosition(descriptions, DEGREE, -currentDegree, DESCRIPTION_CONTAINER_CENTER_X, DESCRIPTION_CONTAINER_CENTER_Y, DESCRIPTION_CONTAINER_RADIUS);
-
-    $$('.rotate').forEach((button, index) => button.onclick = () => rotate(index));
-
-    let targetDegree = currentDegree;
-    let i, rotating = false;
-    function rotate(direction) {
-        targetDegree += direction ? DEGREE : -DEGREE;
-        if (rotating) {
-            return;
-        }
+$$('.rotate').forEach((button) => button.onclick = () => {
+    target += button.classList.contains('right') ? STEP : -STEP;
+    direction = current < target ? 1 : -1;
+    planetImages[visibleImageIndex].classList.remove('visible');
+    planetDescriptions[visibleDescriptionIndex].classList.remove('visible');
+    visibleImageIndex = (visibleImageIndex + (direction * -1) + count) % count;
+    visibleDescriptionIndex = (visibleDescriptionIndex + (-direction * -1) + count) % count;
+    planetImages[visibleImageIndex].classList.add('visible');
+    planetDescriptions[visibleDescriptionIndex].classList.add('visible');
+    if (!rotating) {
         rotating = true;
-        const intervalId = setInterval(() => {
-            if (targetDegree === currentDegree) {
-                clearInterval(intervalId);
-                rotating = false;
-                return;
-            }
-            (targetDegree > currentDegree) ? i = 1 : i = -1;
-            currentDegree += i;
-            planets.forEach((planet) => {
-                const theta = (i + planet.degree) * Math.PI / 180;
-                const x = PLANET_CONTAINER_CENTER_X + PLANET_CONTAINER_RADIUS * Math.cos(theta);
-                const y = PLANET_CONTAINER_CENTER_Y + PLANET_CONTAINER_RADIUS * Math.sin(theta);
-                planet.style.transform = "translate("+x+"px, "+y+"px)";
-                planet.degree += i;
-            });
-            i = -i;
-            descriptions.forEach((description) => {
-                const theta = (i + description.degree) * Math.PI / 180;
-                const x = DESCRIPTION_CONTAINER_CENTER_X + DESCRIPTION_CONTAINER_RADIUS * Math.cos(theta);
-                const y = DESCRIPTION_CONTAINER_CENTER_Y + DESCRIPTION_CONTAINER_RADIUS * Math.sin(theta);
-                description.style.transform = "translate("+x+"px, "+y+"px)";
-                description.degree += i;
-            });
-        }, SPEED);
+        rotate();
+    }
+});
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight' || event.key === 'ArrowLeft') {
+        target += event.key === 'ArrowRight' ? STEP : -STEP;
+        direction = current < target ? 1 : -1;
+        planetImages[visibleImageIndex].classList.remove('visible');
+        planetDescriptions[visibleDescriptionIndex].classList.remove('visible');
+        visibleImageIndex = (visibleImageIndex + (direction * -1) + count) % count;
+        visibleDescriptionIndex = (visibleDescriptionIndex + (-direction * -1) + count) % count;
+        planetImages[visibleImageIndex].classList.add('visible');
+        planetDescriptions[visibleDescriptionIndex].classList.add('visible');
+        if (!rotating) {
+            rotating = true;
+            rotate();
+        }
     }
 });
 
-function addWidthToPlanetsWithCircle(planetWithCircle, planets) {
-    planets.forEach(planet => {
-        if(planetWithCircle.includes(planet.getAttribute('alt'))) {
-            planet.style.width = planet.clientWidth + 30 + 'px';
-        }
-    });
+const setupPosition = (element, i) => {
+    const RADIUS = window.innerWidth / 2;
+    const SIZE = element.clientWidth;
+    const ANGEL = (current + (i * STEP)) * (Math.PI / 180);
+    const x = RADIUS * Math.cos(ANGEL) - SIZE / 2;
+    const y = RADIUS * Math.sin(ANGEL) - SIZE / 2;
+    element.style.transform = "translate("+x+"px, "+y+"px)";
+}
+const setupDescriptionPosition = (element, i) => {
+    const RADIUS = window.innerWidth / 2;
+    const SIZE = element.clientWidth;
+    const ANGEL = (-current + (i * STEP)) * (Math.PI / 180);
+    const x = RADIUS * Math.cos(ANGEL) - SIZE / 2;
+    const y = RADIUS * Math.sin(ANGEL) - SIZE / 2;
+    element.style.transform = "translate("+x+"px, "+y+"px)";
+}
+function rotate() {
+    planetImages.forEach(setupPosition);
+    planetDescriptions.forEach(setupDescriptionPosition);
+    current += direction;
+    if (current === target) {
+        rotating = false;
+        return;
+    }
+    requestAnimationFrame(rotate);
 }
 
-function setupPosition(elements, DEGREE, STARTING_DEGREE, CENTER_X, CENTER_Y, RADIUS) {
-    elements.forEach((element, i) => {
-        element.degree = (DEGREE * i + STARTING_DEGREE);
-        const theta = element.degree * Math.PI / 180;
-        const x = CENTER_X + RADIUS * Math.cos(theta);
-        const y = CENTER_Y + RADIUS * Math.sin(theta);
-        element.style.transform = "translate("+x+"px, "+y+"px)";
-    });
-}
+$('#planets-container').style.top = -(window.innerWidth / 2 - 150) + "px";
+const bottom = window.innerHeight > window.innerWidth ? 250 : 150
+$('#planets-description-container').style.bottom = -(window.innerWidth / 2 - bottom) + "px";
+planetImages.forEach(setupPosition);
+planetDescriptions.forEach(setupDescriptionPosition);
+
 
 
 /* Some default functions to make things simple  (POV: I like the jquery approach) */
